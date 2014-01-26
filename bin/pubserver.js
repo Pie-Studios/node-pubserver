557496c99423acbe66cc157ddc9adbf3a36a427d
Johan Jatko <johan@pie-studios.com>
2014-01-26 20:28:26 +0100

    Body uses all inline data.
    While some might argue that its huge page load, its primarily for
    documentations and such stuff which means it should be possible to save
    it offline without external resources.

--- bin/pubserver.js
+++ bin/pubserver.js
@@ -1,11 +1,18 @@
-var program = require('commander');
-var server = require('../lib/server');
+var program = require('commander'),
+	path 	= require('path');
 
 program
 	.version('0.0.1')
 	.option('-p, --port [portnumber]',"Port to listen on")
 	.option('--host [hostname]', "Hostname to listen on")
-	.option('-s, --serve [folder]', "Folder to serve markdown from")
+	.option('-s, --serve [folder]', "Folder to serve markdown from when running as a server")
+	.option('-r, --prerender [folder]', "Prerender all markdown files in the folder, suitable for static content webservers such as github pages")
 	.parse(process.argv);
 
-server.startServer(program.port, program.host, program.serve);
+if(program.prerender) {
+	var render = require('../lib/prerender');
+	render.prerender(program.prerender);
+} else if(program.port && program.host) {
+	var server = require('../lib/server');
+	server.startServer(program.port, program.host, program.serve || "./webroot");
+}
